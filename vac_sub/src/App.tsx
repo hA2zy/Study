ce889f71ffc3280f1927b4dab21e7d2afa84af4c
hA2zy <128462388+hA2zy@users.noreply.github.com>
2024-02-05 13:58:40 +0900

    공부

---
+++ vac_sub/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Router } from "./router/Router";
+import { GlobalStyle } from "./GlobalStyle/style";
+
+function App() {
+  return (
+    <>
+      <Router />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
